92d722668de34e8703711757e84641709ebb78d9
Oyudo Gerald <gerald.oyudo@gmail.com>
2019-01-31 12:51:01 +0100

    first commit

---
+++ src/main/java/com/gerald/noddus/personcollector/PersonCollectorApplication.java
@@ -0,0 +1,14 @@
+package com.gerald.noddus.personcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonCollectorApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/gerald/noddus/personcollector/PersonCollectorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gerald.noddus.personcollector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonCollectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
